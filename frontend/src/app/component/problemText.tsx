79f1438576effbdeeddd31b50a6df6ce97d7fed3
Quang Nguyen <quang.nguyen5049@gmail.com>
2025-03-24 18:24:11 -0400

    Format Problem Text

--- frontend/src/app/component/problemText.tsx
+++ frontend/src/app/component/problemText.tsx
@@ -13,16 +13,41 @@ export default function ProblemText({
   tags,
   problemID,
 }: ProblemTextProps) {
+  const ParseContent = (text: string) => {
+    text = "<p>" + text + "</p>";
+    console.log(
+      text
+        .trim()
+        .replace(/\s+/g, " ") // Remove extra whitespaces
+        .replace(/\\n+/g, "</p><p>") // Handle line breaks
+        .replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>") // Handle bold
+        .replace(/\[(.*?)\]\((.*?)\)/g, "<a href='$2'>$1</a>")
+        .replace(/\\(\*\*|\[|\]|\\|n)/g, "$1") // Handle links
+        .replace(/\* (.*?)/g, "&#8226;&#9;")
+    );
+    return text
+      .trim()
+      .replace(/\s+/g, " ") // Remove extra whitespaces
+      .replace(/\\n+/g, "</p><p>") // Handle line breaks
+      .replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>") // Handle bold
+      .replace(/\[(.*?)\]\((.*?)\)/g, "<a href='$2'>$1</a>")
+      .replace(/\\(\*\*|\[|\]|\\|n)/g, "$1") // Handle links
+      .replace(/\* (.*?)/g, "&#8226;&#9;"); // Handle bullet points
+  };
   return (
-    <div className="py-5 px-4 flex gap-4 flex-col overflow-y-auto ">
+    <div className="py-5 px-4 flex gap-4 flex-col overflow-y-scroll h-full">
       <div className="title">
-        <h1 className="text-[24px] font-semibold leading-[32px]">
+        <h1 className="text-[24px] font-semibold leading-[32px] text-black">
           {problemID}. {title}
         </h1>
       </div>
       <div className="difficulty text-[12px] leading-[16px] py-1 px-2 bg-[#0000000f] rounded-full text-teal-400 w-fit">
         {difficulty}
       </div>
+      <div
+        dangerouslySetInnerHTML={{ __html: ParseContent(content) }}
+        className="text-black gap-[1rem] text-[14px] flex flex-col"
+      ></div>
     </div>
   );
 }
