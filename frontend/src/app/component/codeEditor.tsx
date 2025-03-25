2ced84687814d43ffc94c835d442fffe14417a60
Quang Nguyen <quang.nguyen5049@gmail.com>
2025-03-25 01:10:07 -0400

    Finish Result Popup

--- frontend/src/app/component/codeEditor.tsx
+++ frontend/src/app/component/codeEditor.tsx
@@ -4,10 +4,35 @@ import { useState } from "react";
 
 interface CodeEditorProps {
   initialCode: string;
+  TestCode: Function;
+  SubmitCode: Function;
 }
-const CodeEditor = ({ initialCode }: CodeEditorProps) => {
+
+const CodeEditor = ({ initialCode, TestCode, SubmitCode }: CodeEditorProps) => {
+  const [code, setCode] = useState<string>(initialCode);
   return (
-    <div className="h-1/1 border-1 flex flex-col justify-between">
+    <div className="h-1/1 flex flex-col justify-between">
+      <div className="flex justify-between px-3 py-3 border-b">
+        <h1 className="font-bold text-2xl"> Code Editor </h1>
+        <div>
+          <button
+            onClick={() => {
+              TestCode(code);
+            }}
+            className="cursor-pointer self-end py-1.5 font-medium items-center whitespace-nowrap focus:outline-none inline-flex bg-fill-3 bg-[#000a200d] hover:bg-[#000a201a] h-[32px] select-none px-5 text-[12px] leading-[1.25rem] text-sm rounded-lg ml-2"
+          >
+            Run
+          </button>
+          <button
+            onClick={() => {
+              SubmitCode(code);
+            }}
+            className="cursor-pointer self-end py-1.5 font-medium items-center whitespace-nowrap focus:outline-none inline-flex bg-fill-3 bg-[#2db55d] hover:bg-[#269a4f] h-[32px] select-none px-5 text-[12px] leading-[1.25rem] text-white text-sm rounded-lg ml-2"
+          >
+            Submit
+          </button>
+        </div>
+      </div>
       <Editor
         height={"100%"}
         defaultLanguage="python"
@@ -18,16 +43,15 @@ const CodeEditor = ({ initialCode }: CodeEditorProps) => {
             enabled: false,
           },
           autoClosingBrackets: "languageDefined",
+          lineNumbers: "on",
         }}
+        onChange={(value) => {
+          if (value) {
+            setCode(value);
+          }
+        }}
+        className="pt-5"
       />
-      <div className="flex justify-end px-3 pb-3">
-        <button className="cursor-pointer self-end py-1.5 font-medium items-center whitespace-nowrap focus:outline-none inline-flex bg-fill-3 bg-[#000a200d] hover:bg-[#000a201a] h-[32px] select-none px-5 text-[12px] leading-[1.25rem] text-sm rounded-lg ml-2">
-          Run
-        </button>
-        <button className="cursor-pointer self-end py-1.5 font-medium items-center whitespace-nowrap focus:outline-none inline-flex bg-fill-3 bg-[#2db55d] hover:bg-[#269a4f] h-[32px] select-none px-5 text-[12px] leading-[1.25rem] text-white text-sm rounded-lg ml-2">
-          Submit
-        </button>
-      </div>
     </div>
   );
 };
--- frontend/src/app/problem/[id]/page.tsx
+++ frontend/src/app/problem/[id]/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 import ReactGridLayout from "react-grid-layout";
-import { useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import CodeEditor from "@/app/component/codeEditor";
 import "./problem.module.css";
 import Chatbox from "@/app/component/chatBox";
 import ProblemText from "@/app/component/problemText";
+import ResultModal from "@/app/component/resultModal";
 
 interface ProblemResponse {
   code_template: string;
@@ -14,6 +15,45 @@ interface ProblemResponse {
   tags: Array<string>;
   title: string;
 }
+
+const GetProblem = async (
+  setProblem: Dispatch<SetStateAction<ProblemResponse>>
+) => {
+  //TODO: retrieve problem details via API
+  setProblem({
+    code_template: "def removeDuplicates(nums):",
+
+    content:
+      "Given an integer array `nums` sorted in **non-decreasing order**, remove the duplicates [**in-place**](https://en.wikipedia.org/wiki/In-place_algorithm) such that each unique element appears only **once**. The **relative order** of the elements should be kept the **same**. Then return _the number of unique elements in_ `nums`.\\n\\nConsider the number of unique elements of `nums` be `k`, to get accepted, you need to do the following things:\\n\\n*   Change the array `nums` such that the first `k` elements of `nums` contain the unique elements in the order they were present in `nums` initially. The remaining elements of `nums` are not important as well as the size of `nums`.\\n*   Return `k`.\\n\\n**Custom Judge:**\\n\\nThe judge will test your solution with the following code:\\n\\nint\\[\\] nums = \\[...\\]; // Input array\\nint\\[\\] expectedNums = \\[...\\]; // The expected answer with correct length\\n\\nint k = removeDuplicates(nums); // Calls your implementation\\n\\nassert k == expectedNums.length;\\nfor (int i = 0; i < k; i++) {\\n    assert nums\\[i\\] == expectedNums\\[i\\];\\n}\\n\\nIf all assertions pass, then your solution will be **accepted**.\\n\\n**Example 1:**\\n\\n**Input:** nums = \\[1,1,2\\]\\n**Output:** 2, nums = \\[1,2,\\_\\]\\n**Explanation:** Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.\\nIt does not matter what you leave beyond the returned k (hence they are underscores).\\n\\n**Example 2:**\\n\\n**Input:** nums = \\[0,0,1,1,1,2,2,3,3,4\\]\\n**Output:** 5, nums = \\[0,1,2,3,4,\\_,\\_,\\_,\\_,\\_\\]\\n**Explanation:** Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.\\nIt does not matter what you leave beyond the returned k (hence they are underscores).\\n\\n**Constraints:**\\n\\n*   `1 <= nums.length <= 3 * `10^4``\\n*   `-100 <= nums[i] <= 100`\\n*   `nums` is sorted in **non-decreasing** order.",
+
+    difficulty: "Easy",
+
+    problem_id: 26,
+
+    tags: ["Two Pointers"],
+
+    title: "Remove Duplicates from Sorted Array",
+  });
+};
+
+const TestCodeServer = (
+  code: string,
+  setResult: Dispatch<SetStateAction<{}>>
+) => {
+  //TODO: Call API to test code
+};
+
+const SubmitCodeServer = (
+  code: string,
+  setResult: Dispatch<SetStateAction<Object>>
+) => {
+  // TODO: Call API to submit code
+};
+
+const StartProgress = () => {
+  // TODO: Call API to start progress
+};
+
 export default function Problem({
   params,
 }: {
@@ -21,6 +61,17 @@ export default function Problem({
 }) {
   const [windowSize, setWindowSize] = useState({ width: 0, height: 0 });
   const [displayChatbox, setDisplayChatbox] = useState(false);
+  const [displayResult, setDisplayResult] = useState(true);
+  const [result, setResult] = useState<any>({
+    error_message: {
+      error: "Expected 2, but got 1",
+      input: "nums = [1, 1, 2]",
+      result: 0,
+    },
+    pass: 0,
+    submission_id: 38,
+    test_cases: 2,
+  });
   const [problem, setProblem] = useState<ProblemResponse>({
     content: "",
     code_template: "",
@@ -29,41 +80,15 @@ export default function Problem({
     tags: [],
     title: "",
   });
-
-  const GetProblem = async () => {
-    //TODO: retrieve problem details via API
-    setProblem({
-      code_template: "def removeDuplicates(nums):",
-
-      content:
-        "Given an integer array `nums` sorted in **non-decreasing order**, remove the duplicates [**in-place**](https://en.wikipedia.org/wiki/In-place_algorithm) such that each unique element appears only **once**. The **relative order** of the elements should be kept the **same**. Then return _the number of unique elements in_ `nums`.\\n\\nConsider the number of unique elements of `nums` be `k`, to get accepted, you need to do the following things:\\n\\n*   Change the array `nums` such that the first `k` elements of `nums` contain the unique elements in the order they were present in `nums` initially. The remaining elements of `nums` are not important as well as the size of `nums`.\\n*   Return `k`.\\n\\n**Custom Judge:**\\n\\nThe judge will test your solution with the following code:\\n\\nint\\[\\] nums = \\[...\\]; // Input array\\nint\\[\\] expectedNums = \\[...\\]; // The expected answer with correct length\\n\\nint k = removeDuplicates(nums); // Calls your implementation\\n\\nassert k == expectedNums.length;\\nfor (int i = 0; i < k; i++) {\\n    assert nums\\[i\\] == expectedNums\\[i\\];\\n}\\n\\nIf all assertions pass, then your solution will be **accepted**.\\n\\n**Example 1:**\\n\\n**Input:** nums = \\[1,1,2\\]\\n**Output:** 2, nums = \\[1,2,\\_\\]\\n**Explanation:** Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.\\nIt does not matter what you leave beyond the returned k (hence they are underscores).\\n\\n**Example 2:**\\n\\n**Input:** nums = \\[0,0,1,1,1,2,2,3,3,4\\]\\n**Output:** 5, nums = \\[0,1,2,3,4,\\_,\\_,\\_,\\_,\\_\\]\\n**Explanation:** Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.\\nIt does not matter what you leave beyond the returned k (hence they are underscores).\\n\\n**Constraints:**\\n\\n*   `1 <= nums.length <= 3 * `10^4``\\n*   `-100 <= nums[i] <= 100`\\n*   `nums` is sorted in **non-decreasing** order.",
-
-      difficulty: "Easy",
-
-      problem_id: 26,
-
-      tags: ["Two Pointers"],
-
-      title: "Remove Duplicates from Sorted Array",
-    });
-    console.log(
-      parseCustomMarkdown(
-        "Given an integer array `nums` sorted in **non-decreasing order**, remove the duplicates [**in-place**](https://en.wikipedia.org/wiki/In-place_algorithm) such that each unique element appears only **once**. The **relative order** of the elements should be kept the **same**. Then return _the number of unique elements in_ `nums`.\\n\\nConsider the number of unique elements of `nums` be `k`, to get accepted, you need to do the following things:\\n\\n*   Change the array `nums` such that the first `k` elements of `nums` contain the unique elements in the order they were present in `nums` initially. The remaining elements of `nums` are not important as well as the size of `nums`.\\n*   Return `k`.\\n\\n**Custom Judge:**\\n\\nThe judge will test your solution with the following code:\\n\\nint\\[\\] nums = \\[...\\]; // Input array\\nint\\[\\] expectedNums = \\[...\\]; // The expected answer with correct length\\n\\nint k = removeDuplicates(nums); // Calls your implementation\\n\\nassert k == expectedNums.length;\\nfor (int i = 0; i < k; i++) {\\n    assert nums\\[i\\] == expectedNums\\[i\\];\\n}\\n\\nIf all assertions pass, then your solution will be **accepted**.\\n\\n**Example 1:**\\n\\n**Input:** nums = \\[1,1,2\\]\\n**Output:** 2, nums = \\[1,2,\\_\\]\\n**Explanation:** Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.\\nIt does not matter what you leave beyond the returned k (hence they are underscores).\\n\\n**Example 2:**\\n\\n**Input:** nums = \\[0,0,1,1,1,2,2,3,3,4\\]\\n**Output:** 5, nums = \\[0,1,2,3,4,\\_,\\_,\\_,\\_,\\_\\]\\n**Explanation:** Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.\\nIt does not matter what you leave beyond the returned k (hence they are underscores).\\n\\n**Constraints:**\\n\\n*   `1 <= nums.length <= 3 * `10^4``\\n*   `-100 <= nums[i] <= 100`\\n*   `nums` is sorted in **non-decreasing** order."
-      )
-    );
+  const TestCode = (code: string) => {
+    TestCodeServer(code, setResult);
   };
-  const parseCustomMarkdown = (text: string) => {
-    return text
-      .trim()
-      .replace(/\s+/g, " ")
-      .replace(/\\n+/g, "</p><p>") // Handle escapes
-      .replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>") // Bold
-      .replace(/\[(.*?)\]\((.*?)\)/g, "<a href='$2'>$1</a>")
-      .replace(/\\(\*\*|\[|\]|\\|n)/g, "$1") // Links;
-      .replace(/\* (.*?)/g, "&#8226;");
+
+  const SubmitCode = (code: string) => {
+    SubmitCodeServer(code, setResult);
   };
   useEffect(() => {
-    GetProblem();
+    GetProblem(setProblem);
 
     setWindowSize({ width: window.innerWidth, height: window.innerHeight });
 
@@ -75,13 +100,24 @@ export default function Problem({
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+  useEffect(() => {
+    if (!displayResult) {
+      setResult({});
+    }
+  }, [displayResult]);
   const layout = [
     { i: "Problem Details", x: 0, y: 0, w: 1, h: 2, static: true },
-    { i: "Code Editor", x: 1, y: 0, w: 1, h: 1, static: true },
-    { i: "Result", x: 2, y: 1, w: 1, h: 1, static: true },
+    { i: "Code Editor", x: 1, y: 0, w: 1, h: 2, static: true },
   ];
   return (
     <main className="bg-gray-200 overflow-hidden">
+      <ResultModal
+        visible={displayResult}
+        onClose={() => {
+          setDisplayResult(false);
+        }}
+        result={result}
+      />
       <div className="tooltip-container absolute bottom-[30px] right-[100px] z-10">
         <button
           aria-describedby="help-tooltip"
@@ -116,10 +152,11 @@ export default function Problem({
           />
         </div>
         <div className="bg-white p-0 m-0" key={"Code Editor"}>
-          <CodeEditor initialCode={problem.code_template} />
-        </div>
-        <div className="bg-white p-0 m-0" key={"Result"}>
-          <h1> Result </h1>
+          <CodeEditor
+            initialCode={problem.code_template}
+            TestCode={TestCode}
+            SubmitCode={SubmitCode}
+          />
         </div>
       </ReactGridLayout>
       <style jsx>
